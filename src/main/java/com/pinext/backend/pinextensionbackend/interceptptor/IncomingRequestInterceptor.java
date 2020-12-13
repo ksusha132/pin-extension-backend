dd85999e4b1c4a7df16c87e40f98071ff0bed470
Ksenia Erastova <erastova.kd@esphere.ru>
2020-12-14 00:35:39 +0300

    filter logging

--- src/main/java/com/pinext/backend/pinextensionbackend/interceptptor/IncomingRequestInterceptor.java
+++ src/main/java/com/pinext/backend/pinextensionbackend/interceptptor/IncomingRequestInterceptor.java
@@ -1,39 +1,38 @@
-package com.pinext.backend.pinextensionbackend.interceptptor;
-
-import io.micrometer.core.instrument.util.IOUtils;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
-
-@Component
-@Slf4j
-public class IncomingRequestInterceptor extends HandlerInterceptorAdapter {
-    @SneakyThrows
-    @Override
-    public boolean preHandle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler) {
-        HttpServletRequest requestCacheWrapperObject = new ContentCachingRequestWrapper(request);
-        requestCacheWrapperObject.getParameterMap();
-        String requestBody = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
-        log.info("Incoming request {}", requestBody);
-        return true;
-    }
-
-    @Override
-    public void afterCompletion(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler,
-            Exception ex) {
-
-    }
-}
+//package com.pinext.backend.pinextensionbackend.interceptptor;
+//
+//import io.micrometer.core.instrument.util.IOUtils;
+//import lombok.SneakyThrows;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+//import org.springframework.web.util.ContentCachingRequestWrapper;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.nio.charset.StandardCharsets;
+//
+//@Component
+//@Slf4j
+//public class IncomingRequestInterceptor extends HandlerInterceptorAdapter {
+//    @SneakyThrows
+//    @Override
+//    public boolean preHandle(
+//            HttpServletRequest request,
+//            HttpServletResponse response,
+//            Object handler) {
+//        HttpServletRequest requestCacheWrapperObject = new ContentCachingRequestWrapper(request);
+//        requestCacheWrapperObject.getParameterMap();
+//        String requestBody = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
+//        log.info("Incoming request {}", requestBody);
+//        return true;
+//    }
+//
+//    @Override
+//    public void afterCompletion(
+//            HttpServletRequest request,
+//            HttpServletResponse response,
+//            Object handler,
+//            Exception ex) {
+//
+//    }
+//}
